ffdfda29b7770c22d3816ce44035f9d85dd96c1c
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-08 22:16:25 +0000

    fechtinh the api response

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 $("#weather-status").on("click", function(){
     console.log("How you dare to click on me!");
 });
-
+var key = 'f132a94011cbf4355115fe11f57a3462';
+let city = "London";
+exclude = "daily";
 async function getWeatherData(){
-const response = await fetch("https://open-meteo.com/");
+const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid="+key);
 const data = await response.json();
 console.log(data);
 };
 
-getWeatherData();
+getWeatherData()
