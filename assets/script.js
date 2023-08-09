2f06e14a8f46743d8794c2ace8ac2b38911fe205
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-09 23:27:35 +0000

    get api data using the fetch method

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
-$("#weather-status").on("click", function(){
-    console.log("How you dare to click on me!");
-});
-var key = 'f132a94011cbf4355115fe11f57a3462';
-let city = "London";
-exclude = "daily";
-async function getWeatherData(){
-const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid="+key);
-const data = await response.json();
-console.log(data);
+const key = 'f132a94011cbf4355115fe11f57a3462';
+
+async function getWeatherData(city) {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ", US,uk&units=metric&appid=" + key);
+    const data = await response.json();
+    console.log(data);
+    updateDOM(city, data);
 };
 
-getWeatherData()
+function updateDOM(city, data) {
+    $("#temperature").text(data.main.temp + " °C");
+    $("#wind").text(Math.floor(data.wind.speed * 3.6) + " Km/h");
+    $("#city-name").text(city);
+};
+
+function searchCity() {
+    let city = $("#city").val();
+    getWeatherData(city);
+};
+
+$(":button").on("click", searchCity);
+
+
