7da31cc78b8a8d029994ba0f1905333dcec53d78
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-10 00:25:03 +0000

    add style to the header

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 const key = 'f132a94011cbf4355115fe11f57a3462';
 
 async function getWeatherData(city) {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ", US,uk&units=metric&appid=" + key);
-    const data = await response.json();
-    console.log(data);
-    updateDOM(city, data);
+    try{
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ", US,uk&units=metric&appid=" + key);
+        const data = await response.json();
+        console.log(data);
+        updateDOM(city, data);  
+    } catch (error) {
+        alert("This is not a valid city name!");
+    };
+
+    
 };
 
 function updateDOM(city, data) {
     $("#temperature").text(data.main.temp + " °C");
     $("#wind").text(Math.floor(data.wind.speed * 3.6) + " Km/h");
-    $("#city-name").text(city);
+    $("#city-name").text(data.name);
 };
 
 function searchCity() {
